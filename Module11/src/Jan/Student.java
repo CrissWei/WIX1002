a844eb5956de48166e29ae146e1b68d76743f4e3
CrissWei <92811234+CrissWei@users.noreply.github.com>
2022-01-19 22:55:03 +0800

    Update Student.java

--- Module11/src/Jan/Student.java
+++ Module11/src/Jan/Student.java
@@ -14,5 +14,10 @@ public class Student {
         System.out.println("第二步：push + http的github仓库地址");
 
         System.out.println("但是提交失败有可能是网络问题，经常打不开github服务器");
+        //直接在github改代码，待会直接去pull到本地电脑去
+        
+    }
+    public void method22(){
+    System.out.println("这个代码是我在github手写的，测试能不能pull到IDEA本地去");
     }
 }

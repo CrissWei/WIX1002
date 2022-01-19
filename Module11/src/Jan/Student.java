74f27051cfbe75283574ee21a6c6baaeebce9f7a
CrissWei <92811234+CrissWei@users.noreply.github.com>
2022-01-19 22:31:19 +0800

    学会了提交到Github,但是网络问题打不开github服务器

--- Module11/src/Jan/Student.java
+++ Module11/src/Jan/Student.java
@@ -10,7 +10,9 @@ public class Student {
     public static void method11(){
         System.out.println("大功告成");
         System.out.println("第一步：右键选git 然后add");
-        System.out.println("第二步：commit files");
+        System.out.println("第二步：commit files或者直接commit and push");
         System.out.println("第二步：push + http的github仓库地址");
+
+        System.out.println("但是提交失败有可能是网络问题，经常打不开github服务器");
     }
 }

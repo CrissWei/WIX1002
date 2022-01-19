d2a3ac75195989a32c5f86c7893cf99d3c6e7ad6
CrissWei <92811234+CrissWei@users.noreply.github.com>
2022-01-20 00:33:13 +0800

    跳过add,直接分开commit和 push

--- Module11/src/Jan/Student.java
+++ Module11/src/Jan/Student.java
@@ -60,6 +60,7 @@ public class Student {
         //成功pushed,,证明确实是网络的问题啊
         System.out.println("不过必须选add");
         System.out.println("也可以先单独，add,然后单独点右上角的commit，然后单独点右上角push,成功啦！");
+        System.out.println("直接跳过add, 直接commit ,然后再push");
     }
 
 }

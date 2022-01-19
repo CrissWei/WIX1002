35ea2ca9c4990e5fa589efb5ef5b843db5ae9993
CrissWei <92811234+CrissWei@users.noreply.github.com>
2022-01-19 22:27:06 +0800

    学会了提交到Github

--- Module11/src/Jan/Student.java
+++ Module11/src/Jan/Student.java
@@ -6,4 +6,11 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("我终于学会了push，需要远程仓库地址http");
     }
+
+    public static void method11(){
+        System.out.println("大功告成");
+        System.out.println("第一步：右键选git 然后add");
+        System.out.println("第二步：commit files");
+        System.out.println("第二步：push + http的github仓库地址");
+    }
 }
